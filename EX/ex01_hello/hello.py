54bb893fc7f819c0f198d0e74a929cd81e9e6e05
danyil <dakurb@ttu.ee>
2021-08-30 23:11:16 +0300

    three in one

---
+++ EX/ex01_hello/hello.py
@@ -0,0 +1,17 @@
+name = input("What is your name? ")
+arv1 = input(f"Hello, {name}! Enter a random number: ")
+arv2 = input("Great! Now enter a second random number: ")
+print(f"{arv1} + {arv2}", "is", int(arv1) + int(arv2))
+
+#2
+'''color = input("What is your favourite color? ")
+plural_noun = input("Enter some plural noun: ")
+verb = input("Enter some verb: ")
+print(f"Roses are {color}, \n{plural_noun} are blue, \nI love to {verb} \nAnd so will you! ")
+'''
+#3
+
+'''greeting = input("Enter a greeting: ")
+recipient = input("Enter a recipient: ")
+times_to_repeat = input("How many times to repeat: ")
+print((greeting + " " + recipient + "! ") * int(times_to_repeat))'''
