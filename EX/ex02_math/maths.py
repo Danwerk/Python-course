a04a8c810d10f37178eb2d698d25e05a3d80973a
danyil <dakurb@ttu.ee>
2021-09-06 22:51:02 +0300

    ready!

--- EX/ex02_math/maths.py
+++ EX/ex02_math/maths.py
@@ -1,7 +1,7 @@
 def ects(ects, weeks):
-    """Implement a function to know how many hours are needed per week."""
     if weeks <= 0:
         print('impossible')
     else:
         hours_per_week = (ects * 26) / weeks
         print(hours_per_week)
+    pass
