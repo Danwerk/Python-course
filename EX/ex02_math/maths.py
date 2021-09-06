dcf3c3fe56f5eb0446f6619fca99412a27e4d746
danyil <dakurb@ttu.ee>
2021-09-06 22:53:33 +0300

    ready!

--- EX/ex02_math/maths.py
+++ EX/ex02_math/maths.py
@@ -1,4 +1,13 @@
 def ects(ects, weeks):
+    """
+        Implement a function to know how many hours are needed per week if each ECTS is 26 hours. If it's not possible in
+        time then return a string "Impossible!".
+
+        Examples:
+        1. ects(30, 12) == 65
+        2. ects(1, 1) == 26
+        3. ects(1, 0) == "Impossible!"
+        """
     if weeks <= 0:
         print('impossible')
     else:
