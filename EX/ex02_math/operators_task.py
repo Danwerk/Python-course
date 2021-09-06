1ef6b3242851495f8e4c780b4d0ef32bb1806356
danyil <dakurb@ttu.ee>
2021-09-06 21:20:00 +0300

    ready!

---
+++ EX/ex02_math/operators_task.py
@@ -0,0 +1,68 @@
+def add(x, y):
+    print(x+y)
+    pass
+
+def sub(x, y):
+    print(x-y)
+    pass
+
+def multiply(x, y):
+    print(x * y)
+    pass
+
+def div(x, y):
+    print(x / y)
+    pass
+
+def modulus(x, y):
+    print(x % y)
+    pass
+
+def floor_div(x, y):
+    print(x // y)
+    pass
+
+def exponent(x, y):
+    print(x ** y)
+    pass
+
+def first_greater_or_equal(x, y):
+    if x >= y:
+        print('True')
+    pass
+
+def second_less_or_equal(x, y):
+    if y <= x:
+        print('True')
+    pass
+
+def x_is_y(x, y):
+    if x == y:
+        print('True')
+    pass
+
+def x_is_not_y(x, y):
+    if x != y:
+        print('True')
+    pass
+
+def if_else(a, b, c, d):
+    result1 = a * b
+    result2 = c / d
+    if result1 > result2:
+        print(result1)
+    elif result1 < result2:
+        print(result2)
+    else:
+        print('0')
+    pass
+
+def surface(length, width):
+    print(length * width)
+    pass
+
+def volume(length, width, height):
+    print(lenght * width * height)
+    pass
+
+
