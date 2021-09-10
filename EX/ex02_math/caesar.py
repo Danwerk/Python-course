eab219f6d0c6522ff2efb8ce74a21fb5347645b8
danyil <dakurb@ttu.ee>
2021-09-10 18:47:37 +0300

    ready!

---
+++ EX/ex02_math/caesar.py
@@ -0,0 +1,24 @@
+ascii_list = []
+crypted = []
+def encode(message: str, shift: int) -> str:
+    for character in message:
+        ascii_list.append(ord(character))
+
+    for i in ascii_list:
+        if i >= 97 and i <= 122:
+            i = i + shift
+            if i > 122:
+                crypted.append((i - 122) + 96)
+            else:
+                crypted.append(i)
+        else:
+            i = i
+            crypted.append(i)
+
+    mystring = ""
+    for char in crypted:
+        mystring = mystring + chr(char)
+    return mystring
+
+
+print(encode("o roqk zaxzrky", 20))
