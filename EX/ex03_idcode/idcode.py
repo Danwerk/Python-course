df80ed7581e50dedeb96bd75f2177592dcfad810
danyil <dakurb@ttu.ee>
2021-09-16 21:51:48 +0300

    yess

--- EX/ex03_idcode/idcode.py
+++ EX/ex03_idcode/idcode.py
@@ -17,7 +17,90 @@ def find_id_code(text: str) -> str:
         return str_number
 
 
+'''
 print(find_id_code(""))
 print(find_id_code("123456789123456789"))
 print(find_id_code("ID code is: 49403136526"))
 print(find_id_code("efs4  9   #4aw0h 3r 1a36g5j2!!6-"))
+'''
+
+
+
+
+
+'''
+b = int(input('enter number: '))
+c = f"{b:03}"
+
+print(c)
+a = f"{9:03}"
+
+'''
+
+
+
+'''
+print(a)
+for i in range(1,11):
+    print(f"{i:03}")
+
+if a < f"{10:03}":
+    print('Kuressaare')
+'''
+
+
+
+'''elif a > f"{10:03}" & a < f"{20:03}":
+    print('Tartu')
+elif a < f"{10:03}":
+    print('Kuressaare')
+if a < f"{10:03}":
+    print('Kuressaare')
+if a < f"{10:03}":
+    print('Kuressaare')
+if a < f"{10:03}":
+    print('Kuressaare')
+if a < f"{10:03}":
+    print('Kuressaare')
+if a < f"{10:03}":
+    print('Kuressaare')
+if a < f"{10:03}":
+    print('Kuressaare')
+if a < f"{10:03}":
+    print('Kuressaare')
+
+
+[] = Kuressaare
+011...020 = Tartu
+021...220 = Tallinn
+221...270 = Kohtla-Järve
+271...370 = Tartu
+371...420 = Narva
+421...470 = Pärnu
+471...710 = Tallinn
+711...999 = undefined
+'''
+
+'''
+def is_valid_gender_number(j):
+    for j in range(0, 9)
+        if j == 0:
+            return False
+    if j > 0 & j <= 6:
+        return True
+    if j == 7 & j == 8:
+        return False
+
+print(is_valid_gender_number(7))
+'''
+
+def funng(i):
+    if i == 0:
+        return False
+    elif i == 7:
+        return False
+    elif i == 8:
+        return False
+    else:
+        return True
+print(funng(9))
