80e34c2a20719342e8b493ff997525cef3d9630e
danyil <dakurb@ttu.ee>
2021-09-24 12:35:49 +0300

    yess

--- EX/ex04_lists/lists.py
+++ EX/ex04_lists/lists.py
@@ -1,8 +1,9 @@
 """Lists."""
 
-
 def generate_list(amount: int, data_type: str) -> list:
     """Return a list with amount elements of type data_type."""
+
+
     dictionary = {
         'list': [],
         'set': set(),
@@ -14,8 +15,23 @@ def generate_list(amount: int, data_type: str) -> list:
     }
     result_list = [dictionary[data_type]] * amount
     return result_list
+print(generate_list(2, 'set'))  # [set(), set()]
+print(generate_list(3, 'string'))  # ["a", "kass", "a"]
+print(generate_list(1, 'list'))  # [[]]
+print(generate_list(5, 'int'))  # [1, 2, 3, 3, 3]
+print()
 
 
+
+
+
+
+
+
+
+
+
+'''
 def generate_combined_list(inputs: list) -> list:
     """
     Write a function that returns a list with the minimal possible length, that still satisfies the criteria below.
@@ -23,17 +39,26 @@ def generate_combined_list(inputs: list) -> list:
     Every element of 'inputs' is a tuple (int amount, string data_type).
     For each element of 'inputs', it must be true that the returned list contains at least 'amount' of elements of type 'data_type'.
     """
-    '''
     for i in inputs:
         if inputs != 'int':
             return generate_list(inputs)
 
         min_list = (max(inputs))
         return generate_list(min_list[0], min_list[1])
-'''
+
 print(generate_combined_list([(3, 'int'), (5, 'int')]))  # [1, 2, 3, 4, 5]
 print(generate_combined_list([(3, 'int'), (5, 'int')]))  # [0, 0, 0, 0, 0]
 print(generate_combined_list([(3, 'int'), (5, 'int'), (4, 'int')]))  # [100, 80, 60, 40, 20]
+'''
+
+
+
+
+
+
+
+
+
 '''
 if __name__ == '__main__':
     # The given outputs are only some of possible outputs, for example for (3, 'string') in the first part an output of ["kass", "koer", "kana"] would also work.
