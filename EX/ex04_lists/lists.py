144da8b36bc9fe23afacf9d68927937741626a16
danyil <dakurb@ttu.ee>
2021-09-25 23:27:07 +0300

    yess

--- EX/ex04_lists/lists.py
+++ EX/ex04_lists/lists.py
@@ -8,12 +8,16 @@ def generate_list(amount: int, data_type: str) -> list:
         'set': set(),
         'tuple': tuple,
         'dict': {},
-        'string': str(),
-        'float': float(),
-        'int': int()
+        'string': [i * 'a' for i in range(1000)],
+        'float': [i*0.1 for i in range(1000)],
+        'int': [i for i in range(1000)]
     }
-    result_list = [dictionary[data_type]] * amount
-    return result_list
+    elem_list = []
+    for i in range(amount):
+        elem_list.append(dictionary[data_type][i])
+    return elem_list
+    #result_list = [dictionary[data_type]] * amount
+    #return result_list
 
 
 def generate_combined_list(inputs: list) -> list:
@@ -35,12 +39,15 @@ def generate_combined_list(inputs: list) -> list:
 
     return output_list
 
-print(generate_combined_list([(3, 'int'), (5, 'int')]))  # [1, 2, 3, 4, 5]
-print(generate_combined_list([(3, 'int'), (5, 'int')]))  # [0, 0, 0, 0, 0]
-print(generate_combined_list([(3, 'int'), (5, 'int'), (4, 'int')]))  # [100, 80, 60, 40, 20]
-print(generate_combined_list([(2, 'list'), (3, 'string')]))  # ["a", [], "a", [], "a"]
-print(generate_combined_list([(2, 'float'), (3, 'dict')]))
 
+def generate_combined_list_unique(inputs: list) -> list:
+    """Write a function that returns a list with the minimal possible length."""
+    return generate_combined_list(inputs)
+
+
+print(generate_combined_list([(3, 'int'), (5, 'int')]))
+print(generate_combined_list_unique([(3, 'int'), (5, 'int')]))  # [1, 2, 3, 4, 5]
+print(generate_combined_list_unique([(2, 'int'), (2, 'float'), (1, 'int')]))
 '''
 if __name__ == '__main__':
     # The given outputs are only some of possible outputs, for example for (3, 'string') in the first part an output of ["kass", "koer", "kana"] would also work.
