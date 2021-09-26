749e51f763c6ee00f309227adf5a49b1f4669eb4
danyil <dakurb@ttu.ee>
2021-09-26 22:50:09 +0300

    yess

--- EX/ex04_lists/lists.py
+++ EX/ex04_lists/lists.py
@@ -48,10 +48,17 @@ def generate_combined_list_unique(inputs: list) -> list:
     return generate_combined_list(inputs)
 
 
+def generate_combined_list_unique_advanced(inputs: list) -> list:
+    """Write a function that returns a list with the minimal possible length."""
+
+    return generate_combined_list(inputs)
 
 
-print(generate_combined_list_unique([(3, 'int'), (5, 'int'), (3, 'string')]))  # [1, 2, 3, 4, 5]
-print(generate_combined_list_unique([(2, 'int'), (2, 'float'), (1, 'int')]))
+print(generate_combined_list_unique_advanced([(3, 'int'), (5, 'int')]))  # [1, 2, 3, 4, 5]
+print(generate_combined_list_unique_advanced([(2, 'list'), (3, 'string')]))  # ["a", [2], "asd", [], "abc"]
+print(generate_combined_list_unique_advanced([(2, 'float'), (3, 'dict')]))  # [{3: "abd"}, {"a": "a"}, {}, 3.14, 3.15]
+
+
 '''
 if __name__ == '__main__':
     # The given outputs are only some of possible outputs, for example for (3, 'string') in the first part an output of ["kass", "koer", "kana"] would also work.
