e824843e3fef495c1448f5d6e9715ea0ef4d60ef
danyil <dakurb@ttu.ee>
2021-09-30 21:58:00 +0300

    yess

---
+++ TK/tk1/exam.py
@@ -0,0 +1,10 @@
+def format_time(minutes: int) -> str:
+    hours = minutes // 60
+    if minutes < 60:
+        return f'{minutes}min'
+    else:
+        minutes = minutes % 60
+        if minutes == 0:
+            return f'{hours}h '
+        else:
+            return f'{hours}h {minutes}min'
