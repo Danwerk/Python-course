d53c225eca44317ab3ea0f7029fecab8f80d7c78
danyil <dakurb@ttu.ee>
2021-09-30 23:56:31 +0300

    yess

--- TK/tk1/exam.py
+++ TK/tk1/exam.py
@@ -17,7 +17,7 @@ def lucky_guess(n: int) -> bool:
         return True
     elif n >= -6 and n <= 121 and n % 13 == 0:
         return True
-    elif n < 0 and '5' not in str(n) and '6' not in str(n):
+    elif n < 0 and '5' not in str_num and '6' not in str_num:
         return True
     else:
         False
@@ -25,7 +25,7 @@ def lucky_guess(n: int) -> bool:
 
 #print(lucky_guess(7))
 #print(lucky_guess(26))
-#print(lucky_guess(-35))
+#print(lucky_guess(32))
 
 
 def sum_of_a_beach(s: str) -> int:
@@ -40,6 +40,43 @@ def sum_of_a_beach(s: str) -> int:
     return count
 
 
-print(sum_of_a_beach("WAtErSlIde"))
 
 
+def index_index_value(nums: list) -> int:
+    """
+    Return value at index.
+
+    Take the last element.
+    Use the last element value as the index to get another value.
+    Use this another value as the index of yet another value.
+    Return this yet another value.
+
+    If the the last element points to out of list, return -1.
+    If the element at the index of last element points out of the list, return -2.
+
+    All elements in the list are non-negative.
+
+    index_index_value([0]) => 0
+    index_index_value([0, 2, 4, 1]) => 4
+    index_index_value([0, 2, 6, 2]) => -2  (6 is too high)
+    index_index_value([0, 2, 4, 5]) => -1  (5 is too high)
+
+    :param nums: List of integer
+    :return: Value at index of value at index of last element's value
+    """
+
+    last_elem = nums[-1]
+    if last_elem > len(nums):
+        return -1
+    new_elem = nums[last_elem]
+    if new_elem > len(nums):
+        return -2
+    else:
+        yet_another_elem = nums[new_elem]
+        return yet_another_elem
+
+
+print(index_index_value([0]))
+print(index_index_value([0, 2, 4, 1]))
+print(index_index_value([0, 2, 6, 2]))
+print(index_index_value([0, 2, 4, 5]))
