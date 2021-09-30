3fb6153e9b894f421698fb13b2acff6bad60f72e
danyil <dakurb@ttu.ee>
2021-09-30 23:31:56 +0300

    yess

--- TK/tk1/exam.py
+++ TK/tk1/exam.py
@@ -9,4 +9,37 @@ def format_time(minutes: int) -> str:
         else:
             return f'{hours}h {minutes}min'
 
-print(format_time(29460))
+
+def lucky_guess(n: int) -> bool:
+    lucky_nums = [1, 3, 7]
+    str_num = str(n)
+    if n in lucky_nums:
+        return True
+    elif n >= -6 and n <= 121 and n % 13 == 0:
+        return True
+    elif n < 0 and '5' not in str(n) and '6' not in str(n):
+        return True
+    else:
+        False
+
+
+#print(lucky_guess(7))
+#print(lucky_guess(26))
+#print(lucky_guess(-35))
+
+
+def sum_of_a_beach(s: str) -> int:
+    low_case = s.lower()
+    count = 0
+    beach_list = ['sand', 'water', 'fish', 'sun']
+    for beach_elem in beach_list:
+        beach_elem_len = len(beach_elem)
+        for i in range(len(s)):
+            if low_case[i:i + beach_elem_len] == beach_elem:
+                count += 1
+    return count
+
+
+print(sum_of_a_beach("WAtErSlIde"))
+
+
