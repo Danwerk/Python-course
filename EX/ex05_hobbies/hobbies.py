ceea667e9a28174c7e7c0d8bb3ad1d67b7792ce1
danyil <dakurb@ttu.ee>
2021-10-01 20:50:23 +0300

    yess

--- EX/ex05_hobbies/hobbies.py
+++ EX/ex05_hobbies/hobbies.py
@@ -12,23 +12,9 @@ def sort_dictionary(dic: dict) -> dict:
         sorted_dic[key] = sorted(dic[key])
     return sorted_dic
 
-'''
-print(sort_dictionary({"b": [], "a": [], "c": []}))
-print(sort_dictionary({"": ["a", "f", "d"]}))
-print(sort_dictionary({"b": ["d", "a"], "a": ["c", "f"]}))
-print(sort_dictionary({"Jack": ["swimming", "hiking"], "Charlie": ["games", "yoga"]}))
-'''
 
 def create_dictionary(data: str) -> dict:
-
-    """
-    Create dictionary about people and their hobbies ie. {name1: [hobby1, hobby2, ...], name2: [...]}.
-
-    There should be no duplicate hobbies on 1 person.
-
-    :param data: given string from database
-    :return: dictionary where keys are people and values are lists of hobbies
-    """
+    """Create dictionary about people and their hobbies ie."""
     dictionary = {}
     pairs_data = data.split("\n")
     for pair in pairs_data:
@@ -109,13 +95,12 @@ def sort_names_and_hobbies(data: str) -> tuple:
     pass
 
 
-
 if __name__ == '__main__':
     sample_data = """Jack:crafting\nPeter:hiking\nWendy:gaming\nMonica:tennis\nChris:origami\nSophie:sport\nMonica:design\nCarmen:sport\nChris:sport\nMonica:skateboarding\nCarmen:cooking\nWendy:photography\nMonica:tennis\nCooper:yoga\nWendy:sport\nCooper:movies\nMonica:theatre\nCooper:yoga\nChris:gaming\nMolly:fishing\nJack:skateboarding\nWendy:fishing\nJack:drawing\nMonica:baking\nSophie:baking\nAlfred:driving\nAlfred:shopping\nAlfred:crafting\nJack:drawing\nCarmen:shopping\nCarmen:driving\nPeter:drawing\nCarmen:shopping\nWendy:fitness\nAlfred:travel\nJack:origami\nSophie:design\nJack:pets\nCarmen:dance\nAlfred:baking\nSophie:sport\nPeter:gaming\nJack:skateboarding\nCooper:football\nAlfred:sport\nCooper:fitness\nChris:yoga\nWendy:football\nMolly:design\nJack:hiking\nMonica:pets\nCarmen:photography\nJack:baking\nPeter:driving\nChris:driving\nCarmen:driving\nPeter:theatre\nMolly:hiking\nWendy:puzzles\nJack:crafting\nPeter:photography\nCarmen:theatre\nSophie:crafting\nCarmen:cooking\nAlfred:gaming\nPeter:theatre\nCooper:hiking\nChris:football\nChris:pets\nJack:football\nMonica:skateboarding\nChris:driving\nCarmen:pets\nCooper:gaming\nChris:hiking\nJack:cooking\nPeter:fishing\nJack:gaming\nPeter:origami\nCarmen:movies\nSophie:driving\nJack:sport\nCarmen:theatre\nWendy:shopping\nCarmen:pets\nWendy:gaming\nSophie:football\nWendy:theatre\nCarmen:football\nMolly:theatre\nPeter:theatre\nMonica:flowers\nMolly:skateboarding\nPeter:driving\nSophie:travel\nMonica:photography\nCooper:cooking\nJack:fitness\nPeter:cooking\nChris:gaming"""
     dic = create_dictionary(sample_data)
-    #print("shopping" in dic["Wendy"])  # -> True
-    #print("fitness" in dic["Sophie"])  # -> False
-    #print("gaming" in dic["Peter"])  # -> True
+    print("shopping" in dic["Wendy"])  # -> True
+    print("fitness" in dic["Sophie"])  # -> False
+    print("gaming" in dic["Peter"])  # -> True
 '''
     print(find_people_with_most_hobbies(sample_data))  # -> ['Jack']
     print(len(dic["Jack"]))  # ->  12
