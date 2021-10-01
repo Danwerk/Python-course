ce176663fb1dd5568c6d7affc3da1f1d9e476956
danyil <dakurb@ttu.ee>
2021-10-01 13:30:49 +0300

    yess

--- TK/tk3/exam.py
+++ TK/tk3/exam.py
@@ -80,20 +80,20 @@ print(non_decreasing_list([12, 12]))
 
 def mirror_ends(s: str) -> str:
     """
-        Given a string, look for a mirror image (backwards) string at both the beginning and end of the given string.
+    Given a string, look for a mirror image (backwards) string at both the beginning and end of the given string.
 
-        In other words, zero or more characters at the very beginning of the given string,
-        and at the very end of the string in reverse order (possibly overlapping).
+    In other words, zero or more characters at the very beginning of the given string,
+    and at the very end of the string in reverse order (possibly overlapping).
 
-        For example, the string "abXYZba" has the mirror end "ab".
+    For example, the string "abXYZba" has the mirror end "ab".
 
-        mirrorEnds("abXYZba") → "ab"
-        mirrorEnds("abca") → "a"
-        mirrorEnds("aba") → "aba"
+    mirrorEnds("abXYZba") → "ab"
+    mirrorEnds("abca") → "a"
+    mirrorEnds("aba") → "aba"
 
-        :param s: String
-        :return: Mirror image string
-        """
+    :param s: String
+    :return: Mirror image string
+    """
 
     new_string = ''
     mirror_string = s[::-1]
@@ -102,5 +102,6 @@ def mirror_ends(s: str) -> str:
             if i == j:
                 new_string += j
     return new_string
-print(mirror_ends("abXYZba"))
 
+
+print(mirror_ends("abXYZba"))
