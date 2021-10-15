cc83fa760c6e1c66b391c0119fb7cf2979682d47
danyil <dakurb@ttu.ee>
2021-10-15 19:53:34 +0300

    yess

--- EX/ex07_files/file_handling.py
+++ EX/ex07_files/file_handling.py
@@ -54,7 +54,7 @@ def write_lines_to_file(filename: str, lines: list) -> None:
     """
     with open(filename, "w") as f:
         for line in lines:
-                f.writelines(line)
+            f.writelines(line)
     return filename
 
 
