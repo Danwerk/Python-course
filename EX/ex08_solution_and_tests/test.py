9a22b1ee61f0150c380f9c9e2852115dc981b5a9
danyil <dakurb@ttu.ee>
2021-10-25 15:37:34 +0300

    yess

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -5,3 +5,13 @@ import solution
 def test_student_study_none():
     res = solution.students_study(0, False)
     assert res is None
+
+
+def test_student_night_coffee_true():
+    assert solution.students_study(20, True)
+
+
+
+def test_student_night_coffee_false():
+    res = solution.students_study(20, False)
+    assert res is True
