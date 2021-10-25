842c8664fe40102b56766a5952d8e3fe9069bde9
danyil <dakurb@ttu.ee>
2021-10-25 17:36:32 +0300

    yess

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -117,3 +117,8 @@ def test_lottery_b_c_same_a_diff():
     res = solution.lottery(2, 3, 2)
     assert input_value == res
 '''
+
+def test_lottery_all_diff():
+    input_value = 1
+    res = solution.lottery(1, 2, 3)
+    assert input_value == res
