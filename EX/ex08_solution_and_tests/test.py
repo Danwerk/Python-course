1bbaff90e23eb2073e834bb8c6b9d8c6fa7fe9ba
danyil <dakurb@ttu.ee>
2021-10-25 16:44:59 +0300

    yess

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -13,21 +13,20 @@ def test_student_evening_coffee_true():
 def test_student_evening_coffee_false():
     assert solution.students_study(20, False)
 
-'''
+
 def test_student_night_coffee_true():
-    assert solution.students_study(3, False)
+    assert solution.students_study(3, False) is not True
 
 
 def test_student_night_coffee_false():
-    assert solution.students_study(3, False)
-'''
+    assert solution.students_study(3, False) is not True
+
 
 def test_student_day_coffee_true():
     assert solution.students_study(6, True)
 
 
-'''
 def test_student_day_coffee_false():
-    assert solution.students_study(6, True)
+    assert solution.students_study(6, False) is not True
 
-'''
+
