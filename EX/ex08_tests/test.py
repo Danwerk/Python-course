f8cfcf53c2b67aa3c3b3e9d95e84b38d910e23f9
danyil <dakurb@ttu.ee>
2021-10-25 12:41:29 +0300

    yess

--- EX/ex08_tests/test.py
+++ EX/ex08_tests/test.py
@@ -23,6 +23,12 @@ def test_part1_str_correct_len():
     assert len(res) == expected_len
 
 
+def test_part1_tuple_correct_len():
+    input_amount = 5
+    res = solution.generate_list(input_amount, "tuple")
+    expected_len = 5
+    assert len(res) == expected_len
+
 def test_part1_int_correct_data_types():
     input_amount = 5
     res = solution.generate_list(input_amount, "int")
