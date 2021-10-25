d7d050872c9721d7af450b5f0526dd43c262e06c
danyil <dakurb@ttu.ee>
2021-10-25 12:29:09 +0300

    yess

---
+++ EX/ex08_tests/test.py
@@ -0,0 +1,25 @@
+import pytest
+import solution
+
+
+def test_part1_int_correct_len():
+    input_amount = 5
+    res = solution.generate_list(input_amount, "int")
+    expected_len = 5
+    assert len(res) == expected_len
+
+
+def test_part1_float_correct_len():
+    input_amount = 5
+    res = solution.generate_list(input_amount, "float")
+    expected_len = 5
+    assert len(res) == expected_len
+
+
+def test_part1_int_correct_data_types():
+    input_amount = 5
+    res = solution.generate_list(input_amount, "int")
+    expected_len = 5
+    assert len(res) == expected_len
+    for element in res:
+        assert isinstance(element, int)
