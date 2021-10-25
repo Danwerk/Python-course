ee4c8b40e9110f11c4601aaac6e81296923d5614
danyil <dakurb@ttu.ee>
2021-10-25 16:35:43 +0300

    yess

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -6,26 +6,28 @@ def test_student_study_none():
     assert solution.students_study(22, False) is not None
 
 
-def test_student_night_coffee_true():
-    assert solution.students_study(20, True)
-
-
-def test_student_night_coffee_false():
-    assert solution.students_study(20, False)
-
-'''
 def test_student_evening_coffee_true():
-    assert solution.students_study(20, False)
+    assert solution.students_study(20, True)
 
 
 def test_student_evening_coffee_false():
     assert solution.students_study(20, False)
 
-'''
+
+def test_student_night_coffee_true():
+    assert solution.students_study(3, False)
+
+
+def test_student_night_coffee_false():
+    assert solution.students_study(3, False)
+
+
 def test_student_day_coffee_true():
     assert solution.students_study(6, True)
 
 
+'''
 def test_student_day_coffee_false():
     assert solution.students_study(6, True)
 
+'''
