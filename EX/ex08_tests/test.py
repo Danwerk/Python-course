48b67da260fc5148708a3be82062c3f6f68e6065
danyil <dakurb@ttu.ee>
2021-10-25 12:32:55 +0300

    yess

--- EX/ex08_tests/test.py
+++ EX/ex08_tests/test.py
@@ -16,6 +16,13 @@ def test_part1_float_correct_len():
     assert len(res) == expected_len
 
 
+def test_part1_str_correct_len():
+    input_amount = 5
+    res = solution.generate_list(input_amount, "string")
+    expected_len = 5
+    assert len(res) == expected_len
+
+
 def test_part1_int_correct_data_types():
     input_amount = 5
     res = solution.generate_list(input_amount, "int")
