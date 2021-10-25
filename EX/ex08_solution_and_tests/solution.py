edcf89d4c0e7d8223ec480b984b718a015d96446
danyil <dakurb@ttu.ee>
2021-10-25 18:52:50 +0300

    yess

--- EX/ex08_solution_and_tests/solution.py
+++ EX/ex08_solution_and_tests/solution.py
@@ -39,9 +39,12 @@ def fruit_order(small_baskets: int, big_baskets: int, ordered_amount: int) -> in
     (4, 1, 9) -> 4
     (3, 1, 10) -> -1
     """
-    big_baskets = ordered_amount // 5
-    if (small_baskets * 1) + (big_baskets * 5) == ordered_amount:
+    if big_baskets > 0 and small_baskets < (big_baskets * 5):
+        big_baskets = ordered_amount // 5
+        small_baskets = ordered_amount - (big_baskets * 5)
         return small_baskets
+    #if (small_baskets * 1) + (big_baskets * 5) == ordered_amount:
+        #return small_baskets
     else:
         return -1
 
@@ -49,4 +52,4 @@ def fruit_order(small_baskets: int, big_baskets: int, ordered_amount: int) -> in
 if __name__ == '__main__':
     # print(students_study(6, False))
     # print(lottery(1, 3, 3))
-    print(fruit_order(9, 1, 14))
+    print(fruit_order(4, 2, 14))
