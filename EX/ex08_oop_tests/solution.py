cf5d3b59aaff4b7664e5dada256c3028db8eafc0
danyil <dakurb@ttu.ee>
2021-11-01 15:06:56 +0200

    yess

--- EX/ex08_oop_tests/solution.py
+++ EX/ex08_oop_tests/solution.py
@@ -1,47 +1,57 @@
 class Factory:
     def __init__(self):
+        """List of cakes."""
         self.cakes = []
 
     def bake_cake(self, toppings: int, base: int) -> int:
+        """Bake cakes and get right amount of cakes."""
         cake = Cake(base, toppings)
         self.cakes.append(cake)
-        # basic_cake = 1
-        # medium_cake = 2
-        # large_cake = 5
-        count = 0
-        if toppings == 1:
-            return 1
-        if 1 < toppings < 5:
-            if toppings % 2 == 0:
-                self.cakes.append(cake)
-                return toppings // 2
 
-        if toppings >= 5:
-            if toppings % 5 == 0:
-                return toppings // 5
+        cakes_amnt = 0
+
+        big_cake = toppings // 5
+        big_cake_amnt = toppings % 5
+        cakes_amnt += big_cake
+
+        medium_cake = big_cake_amnt // 2
+        medium_cake_amnt = big_cake_amnt % 2
+        cakes_amnt += medium_cake
+
+        small_cake = medium_cake_amnt // 1
+        cakes_amnt += small_cake
+
+        return cakes_amnt
 
     def get_last_cakes(self, n: int) -> list:
+        """Get last cakes from cakes list."""
         return self.cakes[-n:]
 
     def get_cakes_baked(self) -> list:
+        """Get cakes baked."""
         return self.cakes
 
     def __str__(self):
+        """Return basic, medium or large."""
         pass
 
 
 class Cake:
 
     def __init__(self, base_amount, toppings_amount):
+        """Class for cake."""
         pass
     @property
     def type(self):
+        """Type of cake."""
         pass
 
     def __repr__(self):
+        """Cakes cakes cakes."""
         pass
 
     def __eq__(self, other):
+        """More cakes."""
         pass
 
 
