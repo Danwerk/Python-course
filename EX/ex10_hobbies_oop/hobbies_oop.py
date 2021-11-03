07fe743598207539905db8bef8b6f8fbf4241a9c
danyil <dakurb@ttu.ee>
2021-11-03 22:42:23 +0200

    yess

--- EX/ex10_hobbies_oop/hobbies_oop.py
+++ EX/ex10_hobbies_oop/hobbies_oop.py
@@ -62,7 +62,6 @@ def sort_by_most_hobbies(people_list: list) -> list:
     :param people_list: list of people to sort.
     :return: sorted list of people.
     """
-
     sorted_people = sorted(people_list, key=lambda p: p.full_name)
     sorted_people = sorted(sorted_people, key=lambda p: len(p.hobbies), reverse=True)
     return sorted_people
@@ -78,7 +77,9 @@ def sort_by_least_hobbies(people_list: list) -> list:
     :param people_list: list of people to sort.
     :return: sorted list of people.
     """
-    pass
+    sorted_people = sorted(people_list, key=lambda p: p.full_name)
+    sorted_people = sorted(sorted_people, key=lambda p: len(p.hobbies))
+    return sorted_people
 
 
 def sort_people_and_hobbies(people_list: list) -> list:
@@ -101,11 +102,11 @@ if __name__ == '__main__':
     person3 = Person("Elon", "Musk", ["late_capitalism", "space", "cars"])
     people = [person1, person2, person3]
 
-   # print(filter_by_hobby(people, "space"))  # -> [JeffBezos, ElonMusk]
+    #print(filter_by_hobby(people, "space"))  # -> [JeffBezos, ElonMusk]
 
-    print(sort_by_most_hobbies(people))  # -> [JeffBezos, ElonMusk, MariKukk]
+    #print(sort_by_most_hobbies(people))  # -> [JeffBezos, ElonMusk, MariKukk]
 
-   # print(sort_by_least_hobbies(people))   # -> [ElonMusk, MariKukk, JeffBezos]
+    print(sort_by_least_hobbies(people))   # -> [ElonMusk, MariKukk, JeffBezos]
 
-   # print(sort_people_and_hobbies(people))  # -> [ElonMusk, JeffBezos, MariKukk]
+    #print(sort_people_and_hobbies(people))  # -> [ElonMusk, JeffBezos, MariKukk]
     #print(person1.hobbies)  # -> ['biking', 'dancing', 'programming']
