3e0143c89c836066247491b30bad49f1a6d8eb67
danyil <dakurb@ttu.ee>
2021-11-21 00:16:24 +0200

    yess

--- EX/ex11_petrol_station/petrol_station.py
+++ EX/ex11_petrol_station/petrol_station.py
@@ -369,7 +369,10 @@ class PetrolStation:
         :param item:
         :param quantity:
         """
-        pass
+        if quantity <= self.__shop_item_stock[item]:
+            self.__shop_item_stock -= quantity
+        else:
+            raise RuntimeError("Not enough!")
 
     def get_fuel_dict(self) -> dict[Fuel, float]:
         """Return dict with Fuel objects as keys and quantities as values."""
