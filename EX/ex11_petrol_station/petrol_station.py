94faa6c9f38e81ffc2650a6277fb750358a9183e
danyil <dakurb@ttu.ee>
2021-11-23 18:56:44 +0200

    yess

--- EX/ex11_petrol_station/petrol_station.py
+++ EX/ex11_petrol_station/petrol_station.py
@@ -130,7 +130,7 @@ class ShopItem(OrderItem):
         if client_type == ClientType.Basic:
             ret = 0
         elif client_type == ClientType.Bronze:
-            ret = 0.075
+            ret = 0.05
         elif client_type == ClientType.Silver:
             ret = 0.1
         elif client_type == ClientType.Gold:
