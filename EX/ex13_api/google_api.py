2caf5010e5da8b1fd0d69645a92b2419659c078c
danyil <dakurb@ttu.ee>
2021-11-24 00:26:07 +0200

    yess

---
+++ EX/ex13_api/google_api.py
@@ -0,0 +1,40 @@
+from __future__ import print_function
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+
+def get_links_from_spreadsheet(id: str, token: str) -> list:
+    """
+    Return a list of strings from the first column of a Google Spreadsheet with the given ID.
+    Example input with https://docs.google.com/spreadsheets/d/1WrCzu4p5lFwPljqZ6tMQEJb2vSJQSGjyMsqcYt-yS4M
+        get_links_from_spreadsheet('1WrCzu4p5lFwPljqZ6tMQEJb2vSJQSGjyMsqcYt-yS4M', 'token.json')
+
+    Returns
+        ['https://www.youtube.com/playlist?list=PLPszdKAlKCXUhU3r25SOFgBxwCEr-JHVS', ... and so on]
+    """
+    sample_spreadsheet_id = id
+    sample_range_name = 'A1:A4'
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists(token):
+        creds = Credentials.from_authorized_user_file(token, SCOPES)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=sample_spreadsheet_id,
+                                range=sample_range_name).execute()
+    values = result.get('values', [])
+
+    flat_list = [item for sublist in values for item in sublist]
+
+    return flat_list
