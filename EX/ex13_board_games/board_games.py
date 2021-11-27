da5ba7a279683166f9fc8af8801858be5a92b386
danyil <dakurb@ttu.ee>
2021-11-27 14:54:18 +0200

    yess

--- EX/ex13_board_games/board_games.py
+++ EX/ex13_board_games/board_games.py
@@ -29,14 +29,14 @@ class Statistics:
         ret.append(self.games)
         return ret
 
-    def get_players_names(self) -> list:
+    def get_player_names(self) -> list:
         """List of players' names."""
         ret = []
         for player in self.players:
             ret.append(player)
         return ret
 
-    def get_games_names(self) -> list:
+    def get_game_names(self) -> list:
         """List of games' names."""
         ret = []
         for game in self.games:
@@ -45,7 +45,7 @@ class Statistics:
 
     def total_played_games(self) -> int:
         """Total amount of played games."""
-        return len(self.get_games_names())
+        return len(self.get_game_names())
 
     def get(self, path: str):
         """Basic getter."""
@@ -78,6 +78,6 @@ if __name__ == '__main__':
     # print(statistics.get('/player/ago/won'))
     # print(statistics.get('/games'))
     print(statistics.read_from_file('ex13_input.txt'))
-    print(statistics.get_players_names())
-    print(statistics.get_games_names())
+    print(statistics.get_player_names())
+    print(statistics.get_game_names())
     print(statistics.total_played_games())
