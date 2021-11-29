5e2add528768232d39565bdc7019f21c64d489c9
danyil <dakurb@ttu.ee>
2021-11-29 12:51:14 +0200

    yess

--- EX/ex13_board_games/board_games.py
+++ EX/ex13_board_games/board_games.py
@@ -7,15 +7,19 @@ class Statistics:
         self.games = {}
         self.all_games = []
         self.all_players = []
+        self.games_played_count = sum(1 for line in open(filename))
         self.filename = self.read_from_file(filename)
 
     def get_file_data(self):
+        """Getter for read from file func."""
         return self.filename
 
     def get_players(self):
+        """Players' dict getter."""
         return self.players
 
     def get_games(self):
+        """Games dict getter."""
         return self.games
 
     def read_from_file(self, filename='ex13_input.txt'):
@@ -27,9 +31,9 @@ class Statistics:
                 elements = line.split(';')
                 game_obj = Game(elements[0])
                 game_str = elements[0]
+
                 if game_str not in self.games:
                     self.games[game_str] = game_obj
-
 
                 people = elements[1].split(',')
                 for player in people:
@@ -48,6 +52,8 @@ class Statistics:
             return self.get_player_names()
         if path == '/games':
             return self.get_game_names()
+        if path == '/total':
+            return self.get_games_played_amount()
 
     def get_player_names(self) -> list:
         """List of players' names."""
@@ -63,8 +69,8 @@ class Statistics:
             ret.append(game)
         return ret
 
-    def get_games_played_amount(self, filename):
-        return sum(1 for line in open(filename))
+    def get_games_played_amount(self):
+        return self.games_played_count
 
     def total_played_games(self) -> int:
         """Total amount of played games."""
@@ -91,10 +97,9 @@ if __name__ == '__main__':
     statistics = Statistics('ex13_input.txt')
     # player = Player('Ago')
     print(statistics.get('/players'))
-    # print(statistics.get('/player/ago/won'))
+    print(statistics.get('/total'))
     print(statistics.get('/games'))
     print(statistics.read_from_file('ex13_input.txt'))
     # print(statistics.get_player_names())
     # print(statistics.get_game_names())
     # print(statistics.total_played_games())
-    print(statistics.get_games_played_amount('ex13_input.txt'))
