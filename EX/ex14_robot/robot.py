c3b711c4269d635b374d009e97824688e3d62a04
danyil <dakurb@ttu.ee>
2021-12-05 15:58:06 +0200

    yess!!

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -57,13 +57,13 @@ def follow_the_line(robot: FollowerBot):
         if robot.get_left_line_sensor() + robot.get_right_line_sensor() + robot.get_second_line_sensor_from_right() + robot.get_second_line_sensor_from_left() == 0:
             robot.set_wheels_speed(100)
             robot.sleep(0.1)
-        elif sum(robot.get_left_line_sensors()) >= 1024 and sum(robot.get_right_line_sensors()) < 1024:
+        elif robot.get_third_line_sensor_from_left() == 1024 and robot.get_third_line_sensor_from_right() == 0:
             robot.set_left_wheel_speed(50)
             robot.set_right_wheel_speed(70)
             robot.sleep(0.1)
 
-        elif sum(robot.get_right_line_sensors()) >= 1024 and sum(robot.get_left_line_sensors()) < 1024:
-            robot.set_right_wheel_speed(55)
+        elif robot.get_third_line_sensor_from_right() == 1024 and robot.get_third_line_sensor_from_left() == 0:
+            robot.set_right_wheel_speed(50)
             robot.set_left_wheel_speed(70)
             robot.sleep(0.1)
         else:
