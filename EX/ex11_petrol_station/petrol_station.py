d5b1a23e8637ee77806e8965026e3271ec57e1ec
danyil <dakurb@ttu.ee>
2021-12-06 19:08:24 +0200

    yess

--- EX/ex11_petrol_station/petrol_station.py
+++ EX/ex11_petrol_station/petrol_station.py
@@ -445,26 +445,27 @@ class PetrolStation:
 
         if not client:
             client = Client('Incognito', total, ClientType.Basic)
-
+        downgrade = False
         for i in items_to_sell:
             if isinstance(i[0], Fuel):
                 if self.__fuel_stock_copy[i[0]] < i[1]:
                     raise RuntimeError('woops')
                 else:
+                    if len(client.get_history()) > 0:
+                        max_range_date = max(d.get_date() for d in client.get_history())
+                        # calculate number of days between two given dates
+                        if (date.today() - max_range_date).days > 60:
+                            downgrade = True
                     order = Order({i[0]: i[1]}, date.today(), client.get_client_type())
                     client.buy(order)
+                    if downgrade:
+                        client.set_client_type(ClientType.Bronze)
+                        client.clear_history()
+
 
             elif isinstance(i[0], ShopItem):
                 if self.__shop_item_stock_copy[i[0]] < i[1]:
                     raise RuntimeError('woops')
-
-
-        else:
-            if len(client.get_history()) > 0:
-                max_range_date = max(d.get_date() for d in client.get_history())
-                # calculate number of days between two given dates
-                if (date.today() - max_range_date).days > 60:
-                    downgrade = True
 
         if client.get_member_balance() >= 1000:
             client.set_client_type(ClientType.Silver)
