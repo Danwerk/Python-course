5356f39369639e8f39eece31229769b3dd853c1b
danyil <dakurb@ttu.ee>
2021-12-07 00:54:16 +0200

    yess!!

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -112,11 +112,6 @@ def the_true_follower(robot: FollowerBot):
 
 
 
-
-
-
-
-
     '''
     while robot.get_right_line_sensors()[0] == 1024 and robot.get_left_line_sensors()[2] == 1024:
         robot.set_wheels_speed(100)
