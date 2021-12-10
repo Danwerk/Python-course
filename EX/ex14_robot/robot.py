00280c7f1815d2215cb1478bdf4a5cec279e95c9
danyil <dakurb@ttu.ee>
2021-12-10 19:05:39 +0200

    yess!!

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -105,17 +105,17 @@ def the_true_follower(robot: FollowerBot):
 
             elif robot.get_third_line_sensor_from_left() == 1024 and robot.get_third_line_sensor_from_right() == 0:
                 robot.set_left_wheel_speed(-75)
-                robot.set_right_wheel_speed(100)
+                robot.set_right_wheel_speed(90)
                 robot.sleep(0.001)
 
             elif robot.get_third_line_sensor_from_right() == 1024 and robot.get_third_line_sensor_from_left() == 0:
-                robot.set_left_wheel_speed(100)
+                robot.set_left_wheel_speed(90)
                 robot.set_right_wheel_speed(-70)
                 robot.sleep(0.001)
 
             else:
                 robot.set_wheels_speed(100)
-                robot.sleep(0.01)
+                robot.sleep(0.001)
                 #print(robot.get_line_sensors())
         print(robot.get_position())
     robot.done()
