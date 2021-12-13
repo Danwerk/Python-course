07fe17aab2737694b930164ebe75512a0672c083
danyil <dakurb@ttu.ee>
2021-12-13 17:54:18 +0200

    yess

--- EXAM/exam0/exam.py
+++ EXAM/exam0/exam.py
@@ -296,11 +296,11 @@ def get_ordered_students(students: list) -> list:
     # list1 = sorted(students, key=lambda p: p.name)
     # list2 = sorted(list1, key=lambda p: p.average_grade)
     # list3 = sorted(list2, key=lambda p: p.credit_points)
+    return sorted(students, key=lambda p: (-p.credit_points, -p.average_grade, p.name))
+    # list1 = sorted(students, key=lambda p: p.credit_points,reverse=True)
+    # list2 = sorted(list1, key=lambda p: p.average_grade, reverse=True)
+    # list3 = sorted(list2, key=lambda p: p.name)
 
-    list1 = sorted(students, key=lambda p: p.credit_points,reverse=True)
-    list2 = sorted(list1, key=lambda p: p.average_grade, reverse=True)
-    list3 = sorted(list2, key=lambda p: p.name)
-    return list3
 
 
 '''
