56172d475967b7068f05df3dde37eb03f71ba537
danyil <dakurb@ttu.ee>
2021-12-27 17:10:33 +0200

    yess

--- XP/xp01_janguru/janguru.py
+++ XP/xp01_janguru/janguru.py
@@ -11,18 +11,21 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2) -> int:
         if pos1 == pos2:
             return pos1
     return -1
-
 '''
-
-"""Janguru."""
 
 
 def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2) -> int:
     """Calculate the meeting position of 2 jangurus."""
-    if pos1 > pos2 and sleep1 < sleep2 or pos2 > pos1 and sleep2 < sleep1:
+    if pos1 and pos2 >=1000000:
+        for time in range(1000000, 1000000000):
+            if time % sleep1 == 0:
+                pos1 += jump_distance1
+            if time % sleep2 == 0:
+                pos2 += jump_distance2
+            if pos1 == pos2:
+                return pos1
         return -1
     else:
-
         for time in range(100000):
             if time % sleep1 == 0:
                 pos1 += jump_distance1
@@ -31,5 +34,3 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2) -> int:
             if pos1 == pos2:
                 return pos1
         return -1
-
-
